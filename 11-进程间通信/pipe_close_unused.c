0e69fe1895ac8888b2e11ebaf9584fb6bd40571e
Peven <iampeven@gmail.com>
2019-04-09 12:26:24 +0800

    Update pipe_close_unused.c

--- 11-进程间通信/pipe_close_unused.c
+++ 11-进程间通信/pipe_close_unused.c
@@ -21,17 +21,17 @@ int main(void)
 	case 0:
 			//子进程执行区域
 			//子进程仅写入数据
-			close(fds[0]);						//关闭复制来的无用的管道出口文件描述符
+			close(fds[0]);				//关闭复制来的无用的管道出口文件描述符
 			write(fds[1], str, sizeof(str));	//向管道入口写入数据
-			close(fds[1]);						//不再使用管道
+			close(fds[1]);				//不再使用管道
 			exit(EXIT_SUCCESS);
 	default:
 			//父进程执行区域
 			//父进程仅读取数据
-			close(fds[1]);								//关闭无用的管道入口文件描述符
-			read(fds[0], buf, BUF_SIZE);				//从管道出口读取数据
+			close(fds[1]);					//关闭无用的管道入口文件描述符
+			read(fds[0], buf, BUF_SIZE);			//从管道出口读取数据
 			printf("Message from child: %s\n", buf);	
-			close(fds[0]);								//不再使用
+			close(fds[0]);					//不再使用
 	}
 
 	return 0;
